aa50b7d5181d561c2da8fc249803cb0cf4f1b9e3
Eetu Haanpää <eehaan@utu.fi>
2023-02-05 16:56:04 +0200

    Alustusta komponenteille

---
+++ todo-app/src/components/Todo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoForm from './TodoForm'
+
+function Todo() {
+    return(
+        <div>
+            <h1>To-do lista</h1>
+            <TodoForm />
+        </div>
+    )
+}
+
+export default Todo;
---
+++ todo-app/src/components/TodoForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function TodoForm() {
+    return (
+        <form>
+            <>
+            <input placeholder='Lisättävä tehtävä'/>
+            <button>+</button>
+            </>
+        </form>
+    )
+}
+
+export default TodoForm
